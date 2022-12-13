9af4ac770992e2602f667b993d2828cd5ab86ad3
DaisyyWard <daisy.w@mmtdigital.co.uk>
2022-12-13 16:07:45 +0000

    Add context

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import AllExpenses from './screens/AllExpenses'
 import { GlobalStyles } from './constants/styles'
 import { Ionicons } from '@expo/vector-icons'
 import IconButton from './UI/IconButton'
+import ExpensesContextProvider from './store/expenses-context'
 
 const Stack = createNativeStackNavigator()
 const BottomTabs = createBottomTabNavigator()
@@ -61,26 +62,28 @@ const ExpensesOverview = () => {
 export default function App() {
   return (
     <>
-      <StatusBar style='auto' />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-            headerTintColor: 'white'
-          }}
-        >
-          <Stack.Screen
-            name='ExpensesOverview'
-            component={ExpensesOverview}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name='ManageExpense'
-            component={ManageExpense}
-            options={{ presentation: 'modal' }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <StatusBar style='light' />
+      <ExpensesContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+              headerTintColor: 'white'
+            }}
+          >
+            <Stack.Screen
+              name='ExpensesOverview'
+              component={ExpensesOverview}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name='ManageExpense'
+              component={ManageExpense}
+              options={{ presentation: 'modal' }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ExpensesContextProvider>
     </>
   )
 }
--- util/date.js
+++ util/date.js
@@ -1,3 +1,8 @@
 export const getFormattedDate = (date) => {
   return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
 }
+
+
+export const getDateMinusDays = (date, days) => {
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days)
+}
